4f9c8e7e410e2a05c5526ab9bc5854b9f292fc3a
Shivam <testdemo7260@gmail.com>
2025-06-13 18:58:03 +0530

    change

--- lib/app/constants/app_urls.dart
+++ lib/app/constants/app_urls.dart
@@ -1,7 +1,7 @@
 class AppUrls{
 
-  static const String baseUrl = 'https://97tzzv24-8000.inc1.devtunnels.ms/driver';
-  // static const String baseUrl = "http://15.207.201.245:8000";
+  // static const String baseUrl = 'https://97tzzv24-8000.inc1.devtunnels.ms/driver';
+  static const String baseUrl = "http://15.207.201.245:8000";
 
   static const String verifyUser = "$baseUrl/verifyUser";
   static const String allAddressUrl = "$baseUrl/user-address";
