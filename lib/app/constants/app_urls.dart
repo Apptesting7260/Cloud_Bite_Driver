2db33258736a29c054765614fb17cc6a822a0492
Apptesting7260 <139454123+Apptesting7260@users.noreply.github.com>
2025-08-20 18:05:15 +0530

    20-08-2025 -- 06:05 PM

--- lib/app/constants/app_urls.dart
+++ lib/app/constants/app_urls.dart
@@ -2,20 +2,20 @@ class AppUrls{
 
   // static const String baseUrl = 'https://97tzzv24-8000.inc1.devtunnels.ms';
   // static const String baseUrl = 'https://api.cloudbitesbw.com';
-  static const String baseUrl = 'http://15.207.201.245:8000'; // live url
+  // static const String baseUrl = 'http://15.207.201.245:8000'; // live url
   // static const String baseUrl = 'https://bpd5zg92-8000.inc1.devtunnels.ms'; // live url
   // static const String baseUrl = 'https://40234xtx-8000.inc1.devtunnels.ms'; // dj
   // static const String baseUrl = 'https://n86h3555-8000.inc1.devtunnels.ms'; // uttam sir
-  // static const String baseUrl = 'https://21qkztxl-8000.inc1.devtunnels.ms';
+  static const String baseUrl = 'https://21qkztxl-8000.inc1.devtunnels.ms';
   static const String imageUrl = "https://cloudbites.s3.af-south-1.amazonaws.com/";
 
   // Socket URL
-  static const String socketURl = 'http://15.207.201.245:8000/'; // live Socket URl
+  // static const String socketURl = 'http://15.207.201.245:8000/'; // live Socket URl
 
   // static const String socketURl = 'https://bpd5zg92-8000.inc1.devtunnels.ms/'; // WFH
 
   // static const String socketURl = 'https://api.cloudbitesbw.com';
-  // static const String socketURl = 'https://21qkztxl-8000.inc1.devtunnels.ms/';
+  static const String socketURl = 'https://21qkztxl-8000.inc1.devtunnels.ms/';
   // static const String socketURl = 'https://n86h3555-8000.inc1.devtunnels.ms/';
 
   static const String verifyUser = "$baseUrl/driver/verifyUser";
