1504fbf58b1f51d03702589c7f12b8a9c907cfc4
Apptesting7260 <testdemo7260@gmail.com>
2025-08-26 18:40:33 +0530

    git check

--- lib/app/modules/sign_up/screens/signup_screen.dart
+++ lib/app/modules/sign_up/screens/signup_screen.dart
@@ -1,22 +1,4 @@
 import 'package:cloud_bites_driver/app/core/app_exports.dart';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class SignUpScreen extends StatelessWidget{
 
   final SignUpController controller = Get.put(SignUpController());
